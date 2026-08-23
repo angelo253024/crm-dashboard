ecf80afa1876ee1f37a23afc7aad3a21863c6a99
angelo253024 <angelo253024@gmail.com>
2026-08-23 17:50:57 -0400

    appmovil

---
+++ public/sw.js
@@ -0,0 +1,44 @@
+﻿const CACHE_NAME = "lavamovil-v1";
+const urlsToCache = [
+  "/",
+  "/index.html"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).catch(() => {
+          // Si no hay red y no está en caché, devolver index.html para que el enrutamiento de React maneje el offline si es posible
+          if (event.request.mode === 'navigate') {
+            return caches.match('/index.html');
+          }
+        });
+      })
+  );
+});
+
+self.addEventListener("activate", event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
